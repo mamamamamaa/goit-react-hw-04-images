ed6bbb9d19657d53c32006d5013d4826b8293d41
Max Denishchuk <maks.denishchuk@gmail.com>
2022-10-14 18:34:23 +0300

    //

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 
 import { ModalContent, Backdrop } from './Modal.styled';
 
 export const Modal = ({ onClose, children }) => {
-  useEffect(() => {
-    window.addEventListener('keydown', onEscapeClose);
-    return () => {
-      window.removeEventListener('keydown', onEscapeClose);
-    };
-  }, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const onEscapeClose = useCallback(e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  });
 
   const onBackdropClose = evt => {
     if (evt.currentTarget === evt.target) {
@@ -18,13 +18,14 @@ export const Modal = ({ onClose, children }) => {
     }
   };
 
-  const onEscapeClose = e => {
-    if (e.code === 'Escape') {
-      onClose();
-    }
-  };
-
   const modalRoot = document.querySelector('#modal-root');
+
+  useEffect(() => {
+    window.addEventListener('keydown', onEscapeClose);
+    return () => {
+      window.removeEventListener('keydown', onEscapeClose);
+    };
+  }, [onEscapeClose]);
 
   return createPortal(
     <Backdrop onClick={onBackdropClose}>
